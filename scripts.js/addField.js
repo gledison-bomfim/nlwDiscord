0952247977b967704121371f2bee4c7c541b4dac
gledison-bomfim <gledisonab@gmail.com>
2020-08-11 17:20:06 -0300

    finish the page give classes at end class thre

---
+++ scripts.js/addField.js
@@ -0,0 +1,22 @@
+//procurar o botão
+//quando clicar no botao
+//executar uma ação 
+    //duplicar campos
+    //colocar na página
+
+
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+function cloneField(){
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    fields.forEach(function(field){
+        field.value = ""
+    })
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+}
