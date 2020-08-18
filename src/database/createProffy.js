9d0f84ff79584e6a35dbc5a0c1cfed682ec48123
gledison-bomfim <gledisonab@gmail.com>
2020-08-18 11:42:09 -0300

    querys

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,4 +1,4 @@
-module.exports = async function(db, {proffyValue, classValue, classScheduleValue}){
+module.exports = async function(db, {proffyValue, classValue, classScheduleValues}){
     
     const insertedProffy = await db.run(`
         INSERT INTO proffys (
@@ -7,14 +7,46 @@ module.exports = async function(db, {proffyValue, classValue, classScheduleValue
             whatsapp,
             bio
         ) VALUES (
-            ${proffyValue.name},
-            ${proffyValue.avatar},
-            ${proffyValue.whatsapp},
-            ${proffyValue.bio}
+            "${proffyValue.name}",
+            "${proffyValue.avatar}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"
         );
     `)
 
-    const proffy_id = insertProffy.lastID
+    const proffy_id = insertedProffy .lastID
+    //inserir dados na tabela classes 
 
-    
+    const insertedClass = await db.run(`
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+            );"
+    `)
+
+    const class_id  = insertedClass.lastID
+
+    const insertedAllclassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+            INSERT INTO class_schedule(
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            )VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+
+            );
+        `)
+    })
+
+    await Promise.all(insertedAllclassScheduleValues)
 }
