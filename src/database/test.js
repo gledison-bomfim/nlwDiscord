f8476e2e50f82003124f40d0343556ea8709a600
gledison-bomfim <gledisonab@gmail.com>
2020-08-20 15:12:49 -0300

    Structuring database

--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ const createProffy = require('./createProffy')
             }
             
             classValue = {
-                subject: "Programação",
+                subject: 1,
                 cost:"80"
             }
 
@@ -32,8 +32,23 @@ const createProffy = require('./createProffy')
             //await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
             const selectedProffys = await db.all("SELECT * FROM proffys")
-            console.log(selectedProffys)
+            //console.log(selectedProffys)
 
-            
+            const selectClassesAndProffys = await db.all(`
+                SELECT classes.*, proffys.*
+                FROM proffys
+                JOIN classes ON (classes.proffy_id = proffys.id) 
+                WHERE classes.proffy_id  = 0;
+            `)
+             //console.log(selectClassesAndProffys)
 
+            const selectClassesSchedules = await db.all(`
+                SELECT class_schedule.*
+                FROM class_schedule
+                WHERE class_schedule.class_id = "1"
+                AND class_schedule.weekday = "0"
+                AND class_schedule.time_from <= "1300"
+                AND class_schedule.time_to > "1300"
+            `)
+            console.log(selectClassesSchedules)
     })
